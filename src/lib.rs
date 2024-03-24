6a6e16aaa7d9bccfff31a1d7d21582df9207a50e
kseniiavi <k8i8v8@gmail.com>
2024-03-24 14:36:31 -0400

    commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,23 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    unimplemented!("square of sum of 1...{n}")
+    let mut sum = 0;
+    for i in 1..n {
+        sum += i
+    }
+    return sum * sum
+    // unimplemented!("square of sum of 1...{n}")
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    unimplemented!("sum of squares of 1...{n}")
+    let mut sums = 0;
+    for i in 1..n {
+        sums += i * i
+    }
+    return sums
+    //unimplemented!("sum of squares of 1...{n}")
 }
 
 pub fn difference(n: u32) -> u32 {
-    unimplemented!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
+    return square_of_sum(n) - sum_of_squares(n);
+    
+    //unimplemented!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
 }
